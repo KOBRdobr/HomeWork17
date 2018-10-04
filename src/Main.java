cc40e17eb6eeca9a39b6b3cc871d6dd5cb401e9e
Nail <cobratms@gmail.com>
2018-10-04 14:18:19 +0300

    Закончено

--- src/Main.java
+++ src/Main.java
@@ -11,36 +11,89 @@ import java.util.Scanner;
  */
 
 public class Main {
+    private static final String LIBRARY_DIR_PATH = "Library";
+
     public static void main(String[] args) throws IOException {
 
-        Scanner in = new Scanner(System.in);
-        boolean key = true;
+        File lib = new File(LIBRARY_DIR_PATH);
+        checkAvailableDir(lib);
 
-        System.out.println("Добро пожаловать в библиотеку!");
-        System.out.println("Что выхотите сделать? (Выбор осуществляется вводом номера нужного действия");
-        System.out.println("1. Добавить книгу");
-        System.out.println("2. Показать список книг");
-        System.out.println("3. Выбрать книгу");
+        welcomeMenu();
 
-        Library lib = new Library();
-        while (key){
-            switch (in.next()) {
-                case "1":
-                    lib.newBook();
-                    key = false;
-                    break;
-                case "2":
-                    lib.listBook();
-                    key = false;
-                    break;
-                case "3":
-                    lib.chooseBook();
-                    key = false;
-                    break;
-                default:
-                    System.out.println("Введите число от 1 до 3!");
+
+    }
+
+    private static void checkAvailableDir(File lib) {
+        if(!lib.isDirectory()) {
+            lib.mkdir();
+        }
+    }
+
+    private static void welcomeMenu() throws IOException {
+
+
+        System.out.println("Welcome to the Library!");
+        System.out.println("What do you want to do?");
+        System.out.println("1. View the list of available books.");
+        System.out.println("2. View a list of genres.");
+        System.out.println("3. Add a book.");
+        System.out.println("4. Delete the book.");
+        System.out.println("5. Exit");
+
+        while(chooseAct()) {
+            System.out.println("\n" + "Enter the number corresponding to the desired act.");
+        }
+    }
+
+    private static boolean chooseAct() throws IOException {
+        Scanner choice = new Scanner(System.in);
+        int numberAct;
+        boolean checkInput = true;
+        boolean exitFlag = true;
+
+        Library libActivities = new Library();
+        while (checkInput) {
+            if(choice.hasNextInt()) {
+                numberAct = choice.nextInt();
+
+                switch (numberAct) {
+                    case 1:
+                        libActivities.viewListBooks();
+                        checkInput = false;
+                        break;
+
+                    case 2:
+                        libActivities.viewListGenres();
+                        checkInput = false;
+                        break;
+
+                    case 3:
+                        libActivities.addBook();
+                        checkInput = false;
+                        break;
+
+                    case 4:
+                        libActivities.deleteBook();
+                        checkInput = false;
+                        break;
+
+                    case 5:
+                        System.out.println("Good Luck!");
+                        checkInput = false;
+                        exitFlag = false;
+                        break;
+
+                    default:
+                        System.out.println("Enter numbers from 1 to 4!");
+                }
+            }
+            else {
+                System.out.println("Enter numbers from 1 to 4!");
+                choice.next();
             }
         }
+
+        return  exitFlag;
     }
 
 }
--- src/ru/zinnurov/Library.java
+++ src/ru/zinnurov/Library.java
@@ -3,95 +3,169 @@ package ru.zinnurov;
 import java.io.*;
 import java.util.Scanner;
 
-
 /**
  * @author Nail Zinnurov
  * cobratms@gmail.com
- * on 20.09.2018
+ * on 03.10.2018
  */
 
 public class Library {
 
-    File libraryDir = new File("library");
+    private static final String LIBRARY_DIR_PATH = "Library";
+    File lib = new File(LIBRARY_DIR_PATH);
 
-    public void newBook() throws IOException {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Какого жанра книга?");
-        String genre = in.nextLine();
-
-        String[] files = libraryDir.list();
-        boolean check = false;
-        for(String search : files) {
-            if(search.equalsIgnoreCase(genre)) {
-                check = true;
-                break;
-            }
-        }
-
-        if (!check) {
-            File newFile = new File("library\\" + genre + ".txt");
-            newFile.createNewFile();
-        }
-        String[] newBook = new String[3];
-        Scanner in2 = new Scanner(System.in);
-
-        System.out.println("Введите назваение книги: ");
-        newBook[0] = in2.nextLine();
-
-        System.out.println("Введите имя автора: ");
-        newBook[1] = in2.nextLine();
-
-        System.out.println("Введите год издания: ");
-        newBook[2] = in2.nextLine();
-        String book = newBook[0] + "_" + newBook[1] + "_" + newBook[2];
-        byte[] bookInBytes = book.getBytes();
+    public void viewListBooks() throws IOException {
 
 
-        if(!checkAvailability(genre, book)) {
-            try (FileWriter stream = new FileWriter("library\\" + genre + ".txt")) {
-                BufferedWriter bw = new BufferedWriter(stream);
-                for (Byte eachByte : bookInBytes) {
-                    bw.write(eachByte);
+        if (lib.list().length != 0) {
+
+            String[] filesPath = lib.list();
+            for (String path : filesPath) {
+
+                try (FileReader file = new FileReader(LIBRARY_DIR_PATH + "//" + path)) {
+                    int symbol;
+                    StringBuilder bookBilder = new StringBuilder();
+
+                    while ((symbol = file.read()) != -1) {
+                        bookBilder.append((char) symbol);
+                    }
+
+                    String books = bookBilder.toString();
+
+                    System.out.println( "Genre: " + path.substring(0, (path.length() - 4)));
+                    System.out.println(books.replace('_', ' '));
+
+                } catch (IOException e) {
+                    throw new IOException("Write Error");
                 }
-                bw.write((byte) '\n');
-                bw.close();
             }
-        }
-        else System.out.println("Такая книга уже есть!");
+        } else System.out.println("Library is empty!");
 
     }
 
-    private boolean checkAvailability(String genre, String book) {
-        boolean check = false;
-        try(FileReader stream = new FileReader("library\\" + genre + ".txt")) {
-            int symbol, k = 0;
-            char[] file = new char[book.length()];
-            while ((symbol = stream.read()) != -1 && k != file.length) {
-                file[k] = (char) symbol;
-                k++;
+    public void viewListGenres() {
+
+        if (lib.list().length != 0) {
+            System.out.println("Genres: ");
+            String[] genres = getGenres();
+
+            for (String genre : genres) {
+                System.out.println(genre);
             }
+        } else System.out.println("Library is empty!");
+    }
 
-            String genreFile =new String(file);
-            String[] books = genreFile.split("\n");
+    private String[] getGenres() {
+        String[] filesPath = lib.list();
+        String[] genre = new String[filesPath.length];
 
-            for(int i = 0; i < books.length; i++) {
-                if(book.equalsIgnoreCase(books[i])) {
-                    System.out.println("Такая книга уже есть!");
-                    check = true;
-                    break;
-                }
+        for(int i = 0; i < filesPath.length; i++) {
+            genre[i] = (filesPath[i].substring(0, (filesPath[i].length() - 4)));
+        }
+
+        return genre;
+    }
+
+    public void addBook() throws IOException {
+        System.out.println("What genre do you want to add a book?");
+
+        Scanner add = new Scanner(System.in);
+        String genre = add.nextLine();
+        checkAvailabilityGenre(genre);
+
+        System.out.println("Name book: ");
+        String nameBook = add.nextLine();
+
+        System.out.println("Author name: ");
+        String nameAuthor = add.nextLine();
+
+        System.out.println("Writing year: ");
+        String writingYear = add.nextLine();
+
+        String book = "\"" + nameBook + "\"_" + nameAuthor + "_" + writingYear + "\n";
+        char[] chars = book.toCharArray();
+
+        try(FileWriter newBook = new FileWriter(LIBRARY_DIR_PATH + "//" + genre + ".txt", true)) {
+            for(char eachChar : chars) {
+                newBook.write(eachChar);
             }
-
-
+            newBook.flush();
         } catch (IOException e) {
-            System.out.println("Пролетел");
+            throw new IOException("Output error");
         }
-        return check;
+
+
+
     }
 
-    public void listBook() {
+    private void checkAvailabilityGenre(String genre) throws IOException {
+
+        String[] genres = getGenres();
+        boolean checkAvailability = false;
+
+        for(String availability : genres ) {
+            if(genre.equalsIgnoreCase(availability)) {
+                checkAvailability = true;
+            }
+        }
+
+        if(!checkAvailability) {
+            createNewFile(genre);
+        }
     }
 
-    public void chooseBook() {
+    private void createNewFile(String newGenre) throws IOException {
+        try {
+            File newFile = new File(LIBRARY_DIR_PATH + "//" + newGenre + ".txt");
+            newFile.createNewFile();
+        } catch (IOException e) {
+            throw new IOException("Error ctreate file");
+        }
+
+    }
+
+    public void deleteBook() throws IOException {
+        if (lib.list().length != 0) {
+            System.out.println("What book do you want to remove?");
+            Scanner deleteBook = new Scanner(System.in);
+            String selectedBook = deleteBook.nextLine();
+            boolean bookExist = false;
+            String[] filesPath = lib.list();
+
+            for (String path : filesPath) {
+                StringBuilder bookBilder = new StringBuilder();
+                try (FileReader file = new FileReader(LIBRARY_DIR_PATH + "//" + path)) {
+                    int symbol;
+
+                    while ((symbol = file.read()) != -1) {
+                        bookBilder.append((char) symbol);
+                    }
+
+                } catch (IOException e) {
+                    throw new IOException("Write Error");
+                }
+
+                String[] allFile = bookBilder.toString().split("\n");
+                FileWriter writer = new FileWriter(LIBRARY_DIR_PATH + "//" + path);
+                for(int i = 0; i < allFile.length; i++) {
+                    if(!selectedBook.equalsIgnoreCase(allFile[i].substring(1, (allFile[i].indexOf('_') - 1)))) {
+                        char[] chars = allFile[i].toCharArray();
+                        try {
+                            for(char eachChar : chars) {
+                                writer.write(eachChar);
+                            }
+                            writer.write("\n");
+                            writer.flush();
+                        } catch (IOException e) {
+                            throw new IOException("Output error");
+                        }
+                    } else bookExist = true;
+                }
+                writer.close();
+            }
+            if (bookExist) System.out.println("Successfully");
+            else System.out.println("The book is not in the library");
+        }
+        else System.out.println("Library is empty");
     }
 }
